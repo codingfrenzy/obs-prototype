8320f4e18a64ad5fb3ad7265467d5878db6a6269
gemicci <cem@cem.com>
2015-05-19 17:56:06 -0400

    arranged directories to maven style directory structure. https://maven.apache.org/guides/introduction/introduction-to-the-standard-directory-layout.html

--- src/test/java/com/observability/helloworld/HelloWorldTest.java
+++ src/test/java/com/observability/helloworld/HelloWorldTest.java
@@ -1,12 +1,14 @@
 package com.observability.helloworld;
 
+import static org.junit.Assert.*;
+
 import org.junit.Test;
 
 public class HelloWorldTest {
 
 	@Test
 	public void test() {
-		//fail("Not yet implemented");
+		fail("Not yet implemented");
 	}
 
 }
