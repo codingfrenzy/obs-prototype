c198e988b46f5b737be5f41158f7f0f0f8b6f7e2
gemicci <cem@cem.com>
2015-05-19 21:27:43 -0400

    Injected a bug that findbugs can find.

--- src/main/java/com/observability/helloworld/HelloWorld.java
+++ src/main/java/com/observability/helloworld/HelloWorld.java
@@ -1,8 +1,15 @@
 package com.observability.helloworld;
 
 public class HelloWorld {
-    public static void main (String[] args){
+    public 
+    static void main (String[] args){
         System.out.println("Hello World");
     }
+   
+    public String foundType() {
+
+    	   return this.foundType();
+
+    	}
 
 }
