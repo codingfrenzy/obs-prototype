78ae66521d25de66975c08c59781039eff423867
gemicci <cem@cem.com>
2015-05-21 15:31:48 -0400

    test comment

--- src/main/java/com/observability/helloworld/HelloWorld.java
+++ src/main/java/com/observability/helloworld/HelloWorld.java
@@ -3,7 +3,7 @@ package com.observability.helloworld;
 public class HelloWorld {
     public 
     static void main (String[] args){
-        System.out.println("Hello World");
+        System.out.println("Hello World Test");
     }
    
     public String foundType() {
