2cb0dd02ac1043cde27fde5bd3ee574a9a681c2e
gemicci <cgemici@andrew.cmu.edu>
2015-05-26 18:22:37 -0400

    fix.

--- src/main/java/com/observability/helloworld/HelloWorld.java
+++ src/main/java/com/observability/helloworld/HelloWorld.java
@@ -3,7 +3,7 @@ package com.observability.helloworld;
 public class HelloWorld {
     public 
     static void main (String[] args){
-        System.out.println(Hello World Test");
+        System.out.println("Hello World Test");
     }
    
  
