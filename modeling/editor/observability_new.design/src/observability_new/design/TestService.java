c838946734aaa72cc152e816cbdb81a2f15ed470
varun saravagi <vsaravag@andrew.cmu.edu>
2015-06-09 18:29:11 -0400

    new service

--- modeling/editor/observability_new.design/src/observability_new/design/TestService.java
+++ modeling/editor/observability_new.design/src/observability_new/design/TestService.java
@@ -1,5 +1,9 @@
 package observability_new.design;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EPackage;
@@ -9,9 +13,17 @@ public class TestService {
 	public TestService(){
 		
 	}
+	
 	public String getName(EObject obj){
 		
 		return "Machine";
+	}
+	
+	public List getList(EObject obj){
+		ArrayList<String> arr = new ArrayList<>();
+		arr.add("hi");
+		arr.add("hello");
+		return arr;
 	}
 	
 //	public static void main(String[] args) throws Exception {
