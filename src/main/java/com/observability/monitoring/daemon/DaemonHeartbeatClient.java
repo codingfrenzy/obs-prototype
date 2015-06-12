c44091d6627a801d4f6bb5554b6904eae6dc191e
codingfrenzy <prstsn@gmail.com>
2015-06-12 18:32:01 -0400

    OBS-702# Created 2 threads: 1 of listening and 1 of processing the information.

--- src/main/java/com/observability/monitoring/daemon/DaemonHeartbeatClient.java
+++ src/main/java/com/observability/monitoring/daemon/DaemonHeartbeatClient.java
@@ -22,7 +22,7 @@
 package com.observability.monitoring.daemon;
 
 import java.io.*;
-import java.net.Socket;
+import java.net.*;
 import java.text.SimpleDateFormat;
 import java.util.*;
 
@@ -31,235 +31,245 @@ import java.util.*;
  * 1. Periodically checks if measurements are being collected<br>
  * 2. Periodically sends heart beat message to collectd server. The message
  * depends on the outcome of step 1.<br>
- * 
+ *
  * @author Prasanth Nair<br>
  *         Created June 5 2015<br>
  */
 
 public class DaemonHeartbeatClient extends Thread {
 
-	/**
-	 * IP for the collectd server
-	 */
-	String collectdServerIP;
+    /**
+     * IP for the collectd server
+     */
+    String collectdServerIP;
 
-	/**
-	 * Port of the collectd server
-	 */
-	int collectdServerPort;
+    /**
+     * Port of the collectd server
+     */
+    int collectdServerPort;
 
-	/**
-	 * This daemon's IP. (IP of this machine)
-	 */
-	String currentDaemonIP;
+    /**
+     * This daemon's IP. (IP of this machine)
+     */
+    String currentDaemonIP;
 
-	/**
-	 * Object of Socket class. Used for connecting
-	 */
-	Socket client;
+    /**
+     * Object of Socket class. Used for connecting
+     */
+    Socket client;
 
-	/**
-	 * The metric used to check if measurements are being collected at the right
-	 * interval.
-	 */
-	String metricName = "cpu-idle";
+    /**
+     * The metric used to check if measurements are being collected at the right
+     * interval.
+     */
+    String metricName = "cpu-idle";
 
-	/**
-	 * The frequency (sampling rate) at which measurements are collected.
-	 */
-	int samplingRate = 30;
+    /**
+     * The frequency (sampling rate) at which measurements are collected.
+     */
+    int samplingRate = 30;
 
-	/**
-	 * The epoch time of the system.
-	 */
-	long systemEpoch;
+    /**
+     * The epoch time of the system.
+     */
+    long systemEpoch;
 
-	/*
-	 * Initialize collectd Server IP <br> Depending on where we are storing the
-	 * configuration, it can be taken from there. For now its hard coded.
-	 */
-	public void initCollectdServerIP() {
-		collectdServerIP = "45.55.197.112";
-	}
+    /*
+     * Initialize collectd Server IP <br> Depending on where we are storing the
+     * configuration, it can be taken from there. For now its hard coded.
+     */
+    public void initCollectdServerIP() {
+        collectdServerIP = "45.55.197.112";
+    }
 
-	/*
-	 * Initialize collectd Server port <br> Depending on where we are storing
-	 * the configuration, it can be taken from there. For now its hard coded.
-	 */
-	public void initCollectdServerPort() {
-		collectdServerPort = 52740;
-	}
+    /*
+     * Initialize collectd Server port <br> Depending on where we are storing
+     * the configuration, it can be taken from there. For now its hard coded.
+     */
+    public void initCollectdServerPort() {
+        collectdServerPort = 52740;
+    }
 
-	/*
-	 * Initialize current daemon IP. <br> Depending on where we are storing the
-	 * configuration, it can be taken from there. For now its hard coded.
-	 */
-	public void initCurrentDaemonIP() {
-		currentDaemonIP = "45.55.240.162";
-	}
+    /*
+     * Initialize current daemon IP. <br> Depending on where we are storing the
+     * configuration, it can be taken from there. For now its hard coded.
+     */
+    public void initCurrentDaemonIP() {
+        currentDaemonIP = "45.55.240.162";
+    }
 
-	/*
-	 * Method to get the current date. Used for metric collection CSV file
-	 * name.<br>
-	 * 
-	 * @return String current date in format yyyy-MM-dd
-	 */
-	public String getTodayDate() {
-		Date date = new Date();
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-		return sdf.format(date);
-	}
+    /*
+     * Method to get the current date. Used for metric collection CSV file
+     * name.<br>
+     *
+     * @return String current date in format yyyy-MM-dd
+     */
+    public String getTodayDate() {
+        Date date = new Date();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        return sdf.format(date);
+    }
 
-	/*
-	 * Method to get the file name of the metric CSV file.<br>
-	 * 
-	 * @return String the full absolute path of the file including file name
-	 */
-	public String getMetricFileName() {
-		String date = "-" + getTodayDate();
-		String metricNameAbsolutePath = "/home/owls/collectd/csv/" + currentDaemonIP + "/cpu-0/" + metricName + date;
-		// metricPath =
-		// "/home/owls/collectd_data_20150604/csv/observabilityCassandra1/cpu-0/cpu-idle-2015-06-02";
-		return metricNameAbsolutePath;
-	}
+    /*
+     * Method to get the file name of the metric CSV file.<br>
+     *
+     * @return String the full absolute path of the file including file name
+     */
+    public String getMetricFileName() {
+        String date = "-" + getTodayDate();
+        String metricNameAbsolutePath = "/home/owls/collectd/csv/" + currentDaemonIP + "/cpu-0/" + metricName + date;
+        // metricPath =
+        // "/home/owls/collectd_data_20150604/csv/observabilityCassandra1/cpu-0/cpu-idle-2015-06-02";
+        return metricNameAbsolutePath;
+    }
 
-	/**
-	 * Method to calculate the maximum confidence time-window. The measurement
-	 * collected should be newer than this time-window.
-	 * 
-	 * @return int threshold
-	 */
-	private int getThreshold() {
-		return samplingRate * 2;
-	}
+    /**
+     * Method to calculate the maximum confidence time-window. The measurement
+     * collected should be newer than this time-window.
+     *
+     * @return int threshold
+     */
+    private int getThreshold() {
+        return samplingRate * 2;
+    }
 
-	/**
-	 * Method to verify if a valid measurement is present in the CSV metric
-	 * file.<br>
-	 * A measurement is valid if it is newer than the configured threshold. <br>
-	 * 
-	 * @return boolean Returns true if valid. False if not.
-	 * @throws IOException
-	 */
-	public boolean verifyLatestMetricMeasurement() throws IOException {
-		getMetricFileName();
-		FileInputStream stream = null;
-		stream = new FileInputStream(getMetricFileName());
-		BufferedReader br1 = new BufferedReader(new InputStreamReader(stream));
-		String strLine, temp = null;
+    /**
+     * Method to verify if a valid measurement is present in the CSV metric
+     * file.<br>
+     * A measurement is valid if it is newer than the configured threshold. <br>
+     *
+     * @return boolean Returns true if valid. False if not.
+     * @throws IOException
+     */
+    public boolean verifyLatestMetricMeasurement() throws IOException {
+        getMetricFileName();
+        FileInputStream stream = null;
+        stream = new FileInputStream(getMetricFileName());
+        BufferedReader br1 = new BufferedReader( new InputStreamReader( stream, "UTF-8"));
+        String strLine, temp = null;
 
-		// Read file line by line and save the last line.
-		while (true) {
-			if ((strLine = br1.readLine()) == null) {
-				break;
-			}
-			temp = strLine;
-		}
+        // Read file line by line and save the last line.
+        while (true) {
+            if ((strLine = br1.readLine()) == null) {
+                break;
+            }
+            temp = strLine;
+        }
 
-		// Close the input stream
-		br1.close();
+        // Close the input stream
+        br1.close();
 
-		// System.out.println(metricNameAbsolutePath);
-		// System.out.println(temp);
+        // System.out.println(metricNameAbsolutePath);
+        // System.out.println(temp);
 
-		temp = temp.substring(0, temp.indexOf('.'));
+        temp = temp.substring(0, temp.indexOf('.'));
 
-		systemEpoch = System.currentTimeMillis() / 1000;
-		long metricLatestEpoch = Long.valueOf(temp).longValue();
+        systemEpoch = System.currentTimeMillis() / 1000;
+        long metricLatestEpoch = Long.parseLong(temp);
 
-		int thresholod = getThreshold();
-		if ((systemEpoch - metricLatestEpoch) <= thresholod) {
-			return true;
-		}
-		return false;
-	}
+        int thresholod = getThreshold();
+        if ((systemEpoch - metricLatestEpoch) <= thresholod) {
+            return true;
+        }
+        return false;
+    }
 
-	/**
-	 * Method to send a message to collectd server.<br>
-	 * Currently uses TCP. Should change to UDP.<br>
-	 * 
-	 * @param String
-	 *            message
-	 */
-	public void sendToCollectdServer(String message) {
-		try {
+    /**
+     * Method to send a message to collectd server.<br>
+     * Currently uses TCP. Should change to UDP.<br>
+     *
+     * @param String message
+     */
+    public void sendToCollectdServer(String message) {
+        try {
 
-			Socket client = new Socket(collectdServerIP, collectdServerPort);
+            byte[] buffer = message.getBytes("UTF-8");
+            InetAddress address = InetAddress.getByName(collectdServerIP);
+//            InetAddress receiverAddress = InetAddress.getLocalHost();
+            DatagramPacket packet = new DatagramPacket(buffer, buffer.length, address, collectdServerPort);
 
-			// client.getLocalPort(); // local port
-			// client.getLocalSocketAddress(); // local IP & port
-			// client.getRemoteSocketAddress(); // collectd server IP & port
+            DatagramSocket datagramSocket = new DatagramSocket();
+//            System.out.println("Buffer size " + buffer.length);
+            datagramSocket.send(packet);
 
-			OutputStream outToServer = client.getOutputStream();
-			DataOutputStream out = new DataOutputStream(outToServer);
+            /*
+            Socket client = new Socket(collectdServerIP, collectdServerPort);
+            // client.getLocalPort(); // local port
+            // client.getLocalSocketAddress(); // local IP & port
+            // client.getRemoteSocketAddress(); // collectd server IP & port
 
-			out.writeUTF(message);
-			// InputStream inFromServer = client.getInputStream();
-			// DataInputStream in = new DataInputStream(inFromServer);
-			// System.out.println("Server says " + in.readUTF());
-			client.close();
+            OutputStream outToServer = client.getOutputStream();
+            DataOutputStream out = new DataOutputStream(outToServer);
 
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+            out.writeUTF(message);
+            // InputStream inFromServer = client.getInputStream();
+            // DataInputStream in = new DataInputStream(inFromServer);
 
-	/**
-	 * Method to create the message that will be sent to collectd server.
-	 * Message Protocol :<br>
-	 * <current daemon IP>_<the epoch time of the system>_<METRIC_VERIFICATIONn><br>
-	 * METRIC_VERIFICATION:<br>
-	 * 1 = metric is old<br>
-	 * 2 = metric is ok and latest<br>
-	 * @param boolean is the latest metric verified?
-	 * @return String message
-	 */
-	private String getMessage(boolean metricLatestVerified) {
-		String message = currentDaemonIP;
-		message += "_" + Long.toString(systemEpoch);
-		if (metricLatestVerified) {
-			message += "_2";
-		} else {
-			message += "_1";
-		}
-		return message;
-	}
+            client.close();
+            */
 
-	/**
-	 * Method to verify the measurement and send the message.<br>
-	 * Needs optimization.
-	 */
-	public void run() {
-		while (true) {
-			boolean metricLatestVerified = false;
-			try {
-				metricLatestVerified = verifyLatestMetricMeasurement();
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-			String message = getMessage(metricLatestVerified);
-			sendToCollectdServer(message);
-			System.out.println(systemEpoch + " : " + metricLatestVerified);
+    /**
+     * Method to create the message that will be sent to collectd server.
+     * Message Protocol :<br>
+     * <current daemon IP>_<the epoch time of the system>_<METRIC_VERIFICATIONn><br>
+     * METRIC_VERIFICATION:<br>
+     * 1 = metric is old<br>
+     * 2 = metric is ok and latest<br>
+     *
+     * @param boolean is the latest metric verified?
+     * @return String message
+     */
+    private String getMessage(boolean metricLatestVerified) {
+        String message = currentDaemonIP;
+        message += "_" + Long.toString(systemEpoch);
+        if (metricLatestVerified) {
+            message += "_2";
+        } else {
+            message += "_1";
+        }
+        return message;
+    }
 
-			try {
-				Thread.sleep(30 * 1000);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		}
-	}
+    /**
+     * Method to verify the measurement and send the message.<br>
+     * Needs optimization.
+     */
+    public void run() {
+        while (true) {
+            boolean metricLatestVerified = false;
+            try {
+                metricLatestVerified = verifyLatestMetricMeasurement();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+            String message = getMessage(metricLatestVerified);
+            sendToCollectdServer(message);
+            System.out.println(systemEpoch + " : " + metricLatestVerified);
 
-		DaemonHeartbeatClient t = new DaemonHeartbeatClient();
-		t.initCollectdServerIP();
-		t.initCollectdServerPort();
-		t.initCurrentDaemonIP();
-		t.start();
+            try {
+                Thread.sleep(samplingRate * 1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
-	}
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+        DaemonHeartbeatClient t = new DaemonHeartbeatClient();
+        t.initCollectdServerIP();
+        t.initCollectdServerPort();
+        t.initCurrentDaemonIP();
+        t.start();
+
+    }
 
 }
 
---
+++ src/main/java/com/observability/monitoring/server/DaemonHeartbeatMain.java
@@ -0,0 +1,322 @@
+//**************************************************************************************************//
+/* Observability Project
+ * Copyright 2015 Master of Software Engineering team: Laila Alhmound, Ying (Joel) Gao, Caglayan Gem, Rajat Kapoor, Prasanth Nair, Varun Saravagi
+ * Copyright 2015 Institute for Software Research | School of Computer Science | Carnegie Mellon University
+ * Copyright 2015 Software Engineering Institute
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses/>.
+ */
+//**************************************************************************************************//
+
+package com.observability.monitoring.server;
+
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+/**
+ * DaemonHeartbeatMain is a process that runs in the server. Functionalities<br>
+ * 1. Keeps in a list of daemons that is supposed to send heartbeat.<br>
+ * 2. Listens to all the heartbeats from the daemons.<br>
+ * 3. Cross checks to find out which daemons did not respond and which daemons
+ * is having trouble collecting metrics.<br>
+ *
+ * @author Prasanth Nair<br>
+ *         Created June 5 2015<br>
+ */
+public class DaemonHeartbeatMain implements Runnable {
+
+    /**
+     * List of daemons that is supposed to send the heartbeat.
+     */
+    private HashSet<String> listOfConfiguredDaemons = new HashSet<String>();
+
+    /**
+     * List of daemons that has not responding within the threshold.
+     */
+    private HashMap<String, Long> listOfNotRespondingDaemons = new HashMap<String, Long>();
+
+    /**
+     * List of daemons that has not been collecting measurements within the threshold.
+     */
+    private HashMap<String, Long> listOfNotCollectingDaemons = new HashMap<String, Long>();
+
+    /**
+     * Toggle to switch between 2 HashMaps for collecting information regarding the daemons that sent the heartbeat.
+     */
+    private AtomicBoolean daemonHeartbeatCollectionToggle;
+
+    /**
+     * Sampling rate of this process. It should be same as the daemons.<br>
+     * Currently hard coded to 30.
+     */
+    int samplingRate = 30;
+
+    /**
+     * Filepath string where the log will be stored.
+     */
+    String filePath = "/home/owls/collectd/missingDaemonLog/";
+
+    /**
+     * List of daemons that has send heartbeat for each interval. <br>
+     * There are 2 HashMaps so as to avoid editing information of one while it is being reset.<br>
+     * Allows stable data transfer between 2 threads.<br>
+     * The switching happen based on the boolean toggle.
+     */
+    private HashMap<String, DaemonInfo> listOfDaemonHeartbeatReceived1;
+    private HashMap<String, DaemonInfo> listOfDaemonHeartbeatReceived2;
+
+    /**
+     * Constructor<br>
+     * Intiliazes the reference to the 2 Hashmap objects and the AtomicBoolean object that controls the toggle.
+     * @param HashMap       list1
+     * @param HashMap       list2
+     * @param AtomicBoolean daemonHeartbeatCollectionToggle - This is used for switch between the hashmaps between threads.
+     */
+    DaemonHeartbeatMain(HashMap<String, DaemonInfo> l1, HashMap<String, DaemonInfo> l2, AtomicBoolean daemonHeartbeatCollectionToggle) {
+        listOfDaemonHeartbeatReceived1 = l1;
+        listOfDaemonHeartbeatReceived2 = l2;
+        this.daemonHeartbeatCollectionToggle = daemonHeartbeatCollectionToggle;
+        initListofConfiguredDaemons();
+    }
+
+    /**
+     * Initialize list of daemons that is supposed to send the heartbeats.
+     * Currently hard coded.
+     */
+    private void initListofConfiguredDaemons() {
+        listOfConfiguredDaemons.add("45.55.240.162");
+        listOfConfiguredDaemons.add("128.2.204.246");
+        listOfConfiguredDaemons.add("123.2.204.246");
+    }
+
+    /**
+     * Method to calculate how long to wait for daemons to send heart beat
+     * before declaring them 'dead'.
+     *
+     * @return int threshold
+     */
+    private int getThreshold() {
+        return samplingRate * 4;
+    }
+
+    /**
+     * Method to verify which all daemons have sent the hearbeat and which all
+     * daemons are having trouble collecting metrics.<br>
+     * Calls saveDaemonNotResponding or saveDaemonNotCollectingMetrics as deemed
+     * appropriate.<br>
+     * The hashmap accessed depends on the toggle. <br>
+     * At the end of the method, we toggle the toggle so that the current hashmap can be cleared, meanwhile data will be entered into the other hashmap.
+     */
+    private void verifyDaemonHeartbeat() {
+
+        HashMap<String, DaemonInfo> tempHeartbeatReceived = null;
+        if (daemonHeartbeatCollectionToggle.get()) {
+            tempHeartbeatReceived = listOfDaemonHeartbeatReceived1;
+            System.out.println("Using VH1");
+        } else {
+            tempHeartbeatReceived = listOfDaemonHeartbeatReceived2;
+            System.out.println("Using VH2");
+        }
+
+        System.out.println("Verifying daemons");
+        for (String configuredIp : listOfConfiguredDaemons) {
+
+            boolean foundFlag = false;
+            boolean collecting = false;
+
+            if (tempHeartbeatReceived.containsKey(configuredIp)) {
+                foundFlag = true;
+                collecting = tempHeartbeatReceived.get(configuredIp).metricStatus;
+
+                // clear this daemon from the not collecting/responding stack
+                // this is because, an entry in this stack will only exist if it is within the threshold.
+                // After the threshold, the entry will be logged and cleared.
+                // Therefore, this means that if the key exists in the stack, we can assume that it missed only a part of the threshold so it is allowed.
+                if (listOfNotRespondingDaemons.containsKey(configuredIp)) {
+                    listOfNotRespondingDaemons.remove(configuredIp);
+                }
+                if (listOfNotCollectingDaemons.containsKey(configuredIp)) {
+                    listOfNotCollectingDaemons.remove(configuredIp);
+                }
+
+            }
+            if (!foundFlag) {
+                saveDaemonNotResponding(configuredIp);
+            }
+            if (!collecting) {
+                saveDaemonNotCollectingMetrics(configuredIp);
+            }
+        }
+
+        // toggle and clear the old hashmap
+        daemonHeartbeatCollectionToggle.set(!daemonHeartbeatCollectionToggle.get());
+        tempHeartbeatReceived.clear();
+    }
+
+    /**
+     * Method to log/notify a dead daemon.
+     *
+     * @param ip
+     */
+    private void logDaemonNotResponding(String ip, long time) {
+        // File write or email can be done here
+        writeToFile(ip, time, true);
+
+    }
+
+    /**
+     * Saves the IP of the daemon that is not responding into the corresponding stack.
+     *
+     * @param String ip - IP of the daemon that is not responding
+     */
+    private void saveDaemonNotResponding(String ip) {
+        if (!listOfNotRespondingDaemons.containsKey(ip)) {
+            Long systemEpoch = new Long(System.currentTimeMillis() / 1000);
+            listOfNotRespondingDaemons.put(ip, systemEpoch);
+//            System.out.println("Saving Daemon not responding: " + ip + "date" + systemEpoch.toString());
+        }
+    }
+
+    /**
+     * Method to log/notify daemon having trouble collecting metrics.
+     *
+     * @param ip
+     */
+    private void logDaemonNotCollecting(String ip, long time) {
+        // File write or email can be done here
+        writeToFile(ip, time, false);
+    }
+
+    /**
+     * Saves the IP of the daemon that is not collecting into the corresponding stack.
+     *
+     * @param String ip - IP of the daemon that is not responding
+     */
+    private void saveDaemonNotCollectingMetrics(String ip) {
+        if (!listOfNotCollectingDaemons.containsKey(ip)) {
+            Long systemEpoch = new Long(System.currentTimeMillis() / 1000);
+            listOfNotCollectingDaemons.put(ip, systemEpoch);
+//            System.out.println("Daemon not collecting: " + ip + systemEpoch.toString());
+        }
+    }
+
+    /*
+     * Method to get the current date. Used for metric collection CSV file
+     * name.<br>
+     *
+     * @return String current date in format yyyy-MM-dd
+     */
+    public String getTodayDate() {
+        Date date = new Date();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        return sdf.format(date);
+    }
+
+    private void writeToFile(String ip, long time, boolean responding) {
+
+        String date = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss").format(new java.util.Date(time * 1000));
+
+        String fileName, content;
+        if (responding) {
+            fileName = "NotResponding";
+            content = "not responding";
+        } else {
+            fileName = "NotCollecting";
+            content = "not collecting";
+        }
+
+        String line = "Daemon " + content + ": " + ip + " since " + date + " (" + time + ")";
+        PrintWriter writer = null;
+        try {
+            writer = new PrintWriter(filePath + fileName + "-" + getTodayDate(), "UTF-8");
+            writer.println(line);
+            writer.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Checks every not responding and/or not collecting daemons' timestamp.<br>
+     * If timestamp is older than threshold, then logs it and deletes it from hashmap.
+     */
+    private void processNotRespondingCollectingDaemons() {
+        Iterator entries = listOfNotRespondingDaemons.entrySet().iterator();
+        while (entries.hasNext()) {
+            Map.Entry entry = (Map.Entry) entries.next();
+            String key = entry.getKey().toString();
+            Long value = Long.parseLong(entry.getValue().toString());
+            long systemEpoch = new Long(System.currentTimeMillis() / 1000);
+            if (systemEpoch - value.longValue() > getThreshold()) {
+                logDaemonNotResponding(key, value.longValue());
+                entries.remove();
+            }
+        }
+
+        entries = listOfNotCollectingDaemons.entrySet().iterator();
+        while (entries.hasNext()) {
+            Map.Entry entry = (Map.Entry) entries.next();
+            String key = entry.getKey().toString();
+            Long value = Long.parseLong(entry.getValue().toString());
+            long systemEpoch = new Long(System.currentTimeMillis() / 1000);
+            if (systemEpoch - value.longValue() > getThreshold()) {
+                logDaemonNotCollecting(key, value.longValue());
+                entries.remove();
+            }
+        }
+
+    }
+
+    /**
+     * Starts the thread.<br>
+     * After every samplingRate, verifies the daemon list and then processes the not-responding or not-collecting hashmap.
+     */
+    public void run() {
+        boolean first = true;
+        while (true) {
+            try {
+                if (!first) {
+                    verifyDaemonHeartbeat();
+                    processNotRespondingCollectingDaemons();
+                }
+                Thread.sleep(samplingRate * 1000);
+                first = !first;
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        HashMap<String, DaemonInfo> l1 = new HashMap<String, DaemonInfo>();
+        HashMap<String, DaemonInfo> l2 = new HashMap<String, DaemonInfo>();
+        AtomicBoolean toggle = new AtomicBoolean(true);
+
+        DaemonHeartbeatMain main = new DaemonHeartbeatMain(l1, l2, toggle);
+        Thread t1 = new Thread(main);
+        t1.start();
+
+        DaemonHeartbeatListener listener = new DaemonHeartbeatListener(l1, l2, toggle);
+        Thread t2 = new Thread(listener);
+        t2.start();
+    }
+
+}
