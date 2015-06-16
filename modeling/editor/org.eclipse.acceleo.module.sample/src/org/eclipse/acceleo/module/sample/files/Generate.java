abf30604369af49b26bc919cf2f73015c0920a60
gemicci <cgemici@andrew.cmu.edu>
2015-06-15 20:37:50 -0400

    Recommit acceleo project files

--- modeling/editor/org.eclipse.acceleo.module.sample/src/org/eclipse/acceleo/module/sample/files/Generate.java
+++ modeling/editor/org.eclipse.acceleo.module.sample/src/org/eclipse/acceleo/module/sample/files/Generate.java
@@ -340,9 +340,6 @@ public class Generate extends AbstractAcceleoGenerator {
     @Override
     public void registerPackages(ResourceSet resourceSet) {
         super.registerPackages(resourceSet);
-        if (!isInWorkspace(observability_emf.Observability_emfPackage.class)) {
-            resourceSet.getPackageRegistry().put(observability_emf.Observability_emfPackage.eINSTANCE.getNsURI(), observability_emf.Observability_emfPackage.eINSTANCE);
-        }
         if (!isInWorkspace(org.eclipse.emf.ecore.EcorePackage.class)) {
             resourceSet.getPackageRegistry().put(org.eclipse.emf.ecore.EcorePackage.eINSTANCE.getNsURI(), org.eclipse.emf.ecore.EcorePackage.eINSTANCE);
         }
