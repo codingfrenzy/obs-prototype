ff6075e3a887b851eeffc3559459b86392b8f388
codingfrenzy <prstsn@gmail.com>
2015-07-13 15:53:40 -0400

    OBS-1000# Nanny script to watch over Observability java processes

--- Scripts/ObservabilityNanny.sh
+++ Scripts/ObservabilityNanny.sh
@@ -10,5 +10,24 @@ do
                 echo "MetricDatabaseHandler down. Respawning."
                 java com.observability.monitoring.server.MetricDatabaseHandler $
         fi
+
+        check=`jps | grep DaemonHeartbeatMain`
+        if [ "$check" ]
+        then
+                echo "DaemonHeartbeatMain is running"
+        else
+                echo "DaemonHeartbeatMain down. Respawning."
+                java com.observability.monitoring.server. DaemonHeartbeatMain > MissingDaemon 2>1& $
+        fi
+
+        check=`jps | grep ModelHandler`
+        if [ "$check" ]
+        then
+                echo "ModelHandler is running"
+        else
+                echo "ModelHandler down. Respawning."
+                java com.observability.monitoring.server. ModelHandler 52.6.202.212 8101 > ModelHandler 2>1& $
+        fi
+
         sleep 29
 done
