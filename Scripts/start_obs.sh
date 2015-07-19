d9719f4aabd86c76437c78d65b15fb8e5d72b7b5
Prasanth Nair <prstsn@gmail.com>
2015-07-19 19:56:06 -0400

    OBS-1083# adding Hostname for collectd.conf

--- Scripts/start_obs.sh
+++ Scripts/start_obs.sh
@@ -3,5 +3,6 @@ apt-get -y install curl
 vmip=`curl -s checkip.dyndns.org | sed -e 's/.*Current IP Address: //' -e 's/<.*$//'`
 java com/observability/monitoring/daemon/DaemonManager $vmip 8200 > DaemonManager 2>&1 &
 cd /home/ubuntu/
+#echo "Hostname $vmip" >> collectd.conf
 sh stop_collectd.sh
 sh start_collectd.sh
