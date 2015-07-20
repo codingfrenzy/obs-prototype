76ebfa3a812e7b1456309f91c43e428d53c565bc
Prasanth Nair <prstsn@gmail.com>
2015-07-20 14:52:34 -0400

    OBS-1084# updating setup

--- Scripts/start_obs.sh
+++ Scripts/start_obs.sh
@@ -4,7 +4,9 @@ cd /home/ubuntu/obsv-code/
 #vmip=`curl -s checkip.dyndns.org | sed -e 's/.*Current IP Address: //' -e 's/<.*$//'`
 vmip=$(curl -s https://api.ipify.org)
 
+sudo pkill -f DaemonManager
 java com/observability/monitoring/daemon/DaemonManager $vmip 8200 > DaemonManager 2>&1 &
+
 cd /home/ubuntu/
 #echo "Hostname $vmip" >> collectd.conf
 sh stop_collectd.sh
