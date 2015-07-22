11ecf1ce9bdd20a2bdd3c89dbf87f99cee1b6e03
lalhmoud <lalhmoud@andrew.cmu.edu>
2015-07-22 13:02:09 -0400

    OBS-1128# Complete the development of config file reader

--- src/main/java/com/observability/monitoring/server/ObservabilityCollectdFileOperations.java
+++ src/main/java/com/observability/monitoring/server/ObservabilityCollectdFileOperations.java
@@ -607,6 +607,7 @@ public class ObservabilityCollectdFileOperations {
 	}
 	
     //////////////////// End: Reading Aggregation Configurations ////////////////////
+	
     public static void main(String[] args) {
         System.out.println(ObservabilityCollectdFileOperations.lastModifiedCollectdConf());
         System.out.println(ObservabilityCollectdFileOperations.lastModifiedDaemonIP());
