5db05d85979ce30dec0a77923a91840352007cbe
Prasanth Nair <prstsn@gmail.com>
2015-07-22 21:29:23 -0400

    OBS-1123# automating db startup

--- Scripts/daemon_start.sh
+++ Scripts/daemon_start.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
+# Put below commands in /etc/rc.local:
+# cd /home/ubuntu/
+# rm -f /home/ubuntu/daemon_start.sh
+# wget -q https://github.com/observability/obs-prototype/raw/master/Scripts/daemon_start.sh
+# sh /home/ubuntu/daemon_start.sh redis
+
+# install curl
 apt-get -y install curl
 #vmip=`curl -s checkip.dyndns.org | sed -e 's/.*Current IP Address: //' -e 's/<.*$//'`
 vmip=$(curl -s https://api.ipify.org)
+
+# Update the epoch time
+apt-get -y install ntpdate
+ntpdate -s ntp.ubuntu.com
 
 # Get latest code and Start Daemon Manager
 cd /home/ubuntu/obsv-code/com/observability/monitoring/daemon/
@@ -11,6 +22,8 @@ rm -f *
 wget -q https://github.com/observability/obs-prototype/raw/master/src/main/java/com/observability/monitoring/daemon/DaemonManager.java
 wget -q https://github.com/observability/obs-prototype/raw/master/src/main/java/com/observability/monitoring/daemon/DaemonHeartbeatClient.java
 wget -q https://github.com/observability/obs-prototype/raw/master/src/main/java/com/observability/monitoring/daemon/IDaemonManagerServer.java
+
+# Start DaemonManager
 cd /home/ubuntu/obsv-code/
 javac com/observability/monitoring/daemon/*.java
 java com/observability/monitoring/daemon/DaemonManager $vmip 8200 > DaemonManager 2>&1 &
