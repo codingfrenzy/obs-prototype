01f4d14ee6ecb0ae45a7608133b0e2ddcc558738
gemicci <cgemici@andrew.cmu.edu>
2015-07-25 16:05:15 -0400

    Update site fixes. Externalizing strings did not work. Fallback to static constants.

--- modeling/workspace/observability/src/com/observability/modeling/common/Messages.java
+++ modeling/workspace/observability/src/com/observability/modeling/common/Messages.java
@@ -20,43 +20,33 @@
 //**************************************************************************************************//
 package com.observability.modeling.common;
 
-import org.eclipse.osgi.util.NLS;
-
 /*
  * Auto generated messages class.
  */
-public class Messages extends NLS {
+public class Messages  {
 	
-	private static final String BUNDLE_NAME = "com.observability.modeling.common"; //$NON-NLS-1$
-	public static String SiriusServices_ID_SEPERATOR;
-	public static String SiriusServices_MACHINE;
-	public static String SiriusServices_MSG_ERROR_ECLIPSE_CANNOTBENULL;
-	public static String SiriusServices_MSG_ERROR_MUST_BE_DB_CLUSTER;
-	public static String SiriusServices_MSG_ERROR_NO_DESCRIPTOR_DIR;
-	public static String SiriusServices_MSG_ERROR_NO_DESCRIPTOR_FILES;
-	public static String SiriusServices_NULL;
-	
-	public static String ModelHandler_CONSTANTS_DESCRIPTOR_FILE_NAME;
-	public static String ModelHandler_CONSTANTS_MODEL_HANDLER_RMI_SERVICE_NAME;
-	public static String ModelHandler_MSG_ERROR_INVALID_DIRECTORY;
-	public static String ModelHandler_MSG_ERROR_NO_PERMISSIONS;
-	public static String ModelHandler_MSG_ERROR_GET_DESCRIPTOR;
-	public static String ModelHandler_MSG_ERROR_DESCRIPTOR_UNZIP;
-	public static String ModelHandler_MSG_ERROR_CONNECTION;
-	public static String ModelHandler_MSG_ERROR_NOT_BOUND;
-	public static String ModelHandler_MSG_ERROR_WRONG_URL;
-	public static String ModelHandler_MSG_ERROR_UPLOAD;
-	public static String ModelHandler_MSG_ERROR_UPLOAD_NOT_STARTED;
-	
-	public static String Ui_MSG_INFO_ENTER_IP_PORT;
-	public static String Ui_CONSTANT_SERVER_WINDOW_TITLE;
-	public static String Ui_MSG_INFO_SERVER_CANCEL;
-	public static String Ui_MSG_ERROR_WRONG_INPUT;
-	
-	static {
-		NLS.initializeMessages(BUNDLE_NAME, Messages.class);
-	}
+	public static final String SiriusServices_ID_SEPERATOR="_";
+	public static final String SiriusServices_MACHINE="Machine ";
+	public static final String SiriusServices_MSG_ERROR_ECLIPSE_CANNOTBENULL="Argument cannot be null: EclipseResourceDelegate";
+	public static final String SiriusServices_MSG_ERROR_MUST_BE_DB_CLUSTER="Container that new machine will be added must be of type DatabaseCluster!";
+	public static final String SiriusServices_MSG_ERROR_NO_DESCRIPTOR_DIR="Cannot find descriptor directory <project_dir>/descriptors";
+	public static final String SiriusServices_MSG_ERROR_NO_DESCRIPTOR_FILES="No descriptor files found. Please add the descriptor files and do the operation again.";
+	public static final String SiriusServices_NULL="null";
+	public static final String ModelHandler_MSG_ERROR_INVALID_DIRECTORY="Invalid project directory path.";
+	public static final String ModelHandler_MSG_ERROR_NO_PERMISSIONS="Unable to create descriptors directory. Please check permissions.";
+	public static final String ModelHandler_MSG_ERROR_GET_DESCRIPTOR="Failed to get correct descriptor file from server. Please try again.";
+	public static final String ModelHandler_MSG_ERROR_DESCRIPTOR_UNZIP="Error in unzipping the descriptor files.";
+	public static final String ModelHandler_MSG_ERROR_CONNECTION="Failed to connect to the server. ";
+	public static final String ModelHandler_MSG_ERROR_NOT_BOUND="The service is not bound at"; 
+	public static final String ModelHandler_MSG_ERROR_WRONG_URL="URL is not well-formed. Please check Ip and Port provided.";
+	public static final String ModelHandler_MSG_ERROR_UPLOAD="Error in uploading the file";
+	public static final String ModelHandler_MSG_ERROR_UPLOAD_NOT_STARTED="File upload could not be started";
+	public static final String ModelHandler_CONSTANTS_DESCRIPTOR_FILE_NAME="descriptors.zip";
+	public static final String ModelHandler_CONSTANTS_MODEL_HANDLER_RMI_SERVICE_NAME="ModelHandler";
+	public static final String Ui_MSG_INFO_ENTER_IP_PORT="Enter the IP and port where the Model Handler service is running, in the format -> ip:port";
+	public static final String Ui_CONSTANT_SERVER_WINDOW_TITLE="Server Details";
+	public static final String Ui_MSG_INFO_SERVER_CANCEL="Model creation cannot continue unless the descriptor files are obtained. Do you want to continue?";
+	public static final String Ui_MSG_ERROR_WRONG_INPUT="Input is not in correct format.";
 
-	private Messages() {
-	}
+	private Messages() {}
 }
